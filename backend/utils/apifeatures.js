322ee1ddf5bf937b7e8a5f04ecb08d3803225076
maroti <115974943+maroti-821@users.noreply.github.com>
2022-12-18 12:42:45 +0530

    Added filter by price and rating in filter method

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -25,7 +25,12 @@ class ApiFeatures {
 
         removeFields.forEach(key=>delete queryCopy[key]);
 
-        this.query = this.query.find(queryCopy);
+        // Filter for Price and Rating
+
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g,key => `$${key}`);
+
+        this.query = this.query.find(JSON.parse(queryStr));
         return this;
     }
 };
