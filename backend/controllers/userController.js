9010c68989d082978e33338e35f83adc1c265183
maroti <115974943+maroti-821@users.noreply.github.com>
2022-12-20 13:18:56 +0530

    Added password reset (backend) Created sendEmail.js and used NodeMailer with gmail Added getResetPasswordToken in userModel Added reset password route in userRoute

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,58 +2,99 @@ const ErrorHandler = require("../utils/errrohandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 
 //Register a User
-exports.registerUser = catchAsyncErrors(async (req,res,next) => {
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-    const {name,email,password} = req.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "This is sample id",
+      url: "profilepicUrl",
+    },
+  });
 
-    const user = await User.create({
-        name,email,password,
-        avatar:{
-            public_id:"This is sample id",
-            url:"profilepicUrl"
-        }
-    });
-
-    sendToken(user,201,res);
+  sendToken(user, 201, res);
 });
 
 // Login User
-exports.loginUser = catchAsyncErrors(async(req,res,next) => {
-    const {email,password} = req.body;
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
 
-    // checking if user has given password and email both
-    if(!email || !password){
-        return next(new ErrorHandler("Please Enter Email & Password",400));
-    }
+  // checking if user has given password and email both
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Email & Password", 400));
+  }
 
-    const user = await User.findOne({ email }).select("+password");
+  const user = await User.findOne({ email }).select("+password");
 
-    if(!user){
-        return next(new ErrorHandler("Invalid Email or Password",401));
-    }
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
 
-    const isPasswordMatched = user.comparePassword(password);
+  const isPasswordMatched = user.comparePassword(password);
 
-    if(!isPasswordMatched){
-        return next(new ErrorHandler("Invalid Email or Password",401));
-    }
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
 
-    sendToken(user,200,res);
+  sendToken(user, 200, res);
 });
 
-
 // Logout user
-exports.logout = catchAsyncErrors(async (req,res,next) => {
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
 
-    res.cookie("token",null,{
-        expires:new Date(Date.now()),
-        httpOnly:true,
-    })
+  res.status(200).json({
+    success: true,
+    message: "Logged out",
+  });
+});
+
+// Forgot Passowrd
+
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  // Get ResetPassword Token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\n If you have not requested this email then please ignore it`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Ecommerce Password Recovery`,
+      message,
+    });
 
     res.status(200).json({
-        success:true,
-        message:"Logged out"
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
     });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorHandler(error.message, 500));
+  }
 });
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,70 +1,77 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"Please Enter product Name"],
-        trim:true
+  name: {
+    type: String,
+    required: [true, "Please Enter product Name"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Please Enter product Description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter product Price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
     },
-    description:{
-        type:String,
-        required:[true,"Please Enter product Description"]
+  ],
+  category: {
+    type: String,
+    required: [true, "Please Enter product Category"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please Enter product Stock"],
+    maxLength: [4, "Stock cannot exceed 4 characters"],
+    default: 1,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
     },
-    price:{
-        type:Number,
-        required:[true,"Please Enter product Price"],
-        maxLength:[8,"Price cannot exceed 8 characters"]
-    },
-    rating:{
-        type:Number,
-        default:0
-    },
-    images:[
-        {
-            public_id:{
-                type:String,
-                required:true
-            },
-            url:{
-                type:String,
-                required:true
-            }
-        }
-    ],
-    category:{
-        type:String,
-        required:[true,"Please Enter product Category"]
-    },
-    stock:{
-        type:Number,
-        required:[true,"Please Enter product Stock"],
-        maxLength:[4,"Stock cannot exceed 4 characters"],
-        default:1
-    },
-    numOfReviews:{
-        type:Number,
-        default:0
-    },
-    reviews:[
-        {
-            name:{
-                type:String,
-                required:true,
-            },
-            rating:{
-                type:Number,
-                required:true
-            },
-            comment:{
-                type:String,
-                required:true
-            }
-        }
-    ],
-    createdAt:{
-        type:Date,
-        default:Date.now
-    }
-})
+  ],
 
-module.exports = mongoose.model("product",productSchema); 
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    requires: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("product", productSchema);
