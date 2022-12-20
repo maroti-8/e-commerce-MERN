355470e7e8416a9754a8ff75c828a55b74005d85
maroti <115974943+maroti-821@users.noreply.github.com>
2022-12-20 15:06:48 +0530

    Added Get User Details in User controller Added get User details route in userRoute.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -134,3 +134,12 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 
   sendToken(user, 200, res);
 });
+
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,7 +5,11 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getUserDetails,
 } = require("../controllers/userController");
+
+const { isAuthenticatedUser, authorizedRoles } = require("../middleware/auth");
+
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -16,5 +20,7 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
 
 module.exports = router;
