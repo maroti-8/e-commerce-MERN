93e5f544ca6263ea76329d2f618eb81edd50515a
maroti <115974943+maroti-821@users.noreply.github.com>
2022-12-21 13:55:19 +0530

    Added user Controller features (details below) 1. Update user profile -(Solved bug for validation email and name) 2. Get all users 3. Get single user -Admin 4. Update user profile -Admin 5. delete user -Admin Added all routes to userRoute.js modified admin routes for admin ----------------------------

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router
-  .route("/product/new")
+  .route("/admin/product/new")
   .post(
     isAuthenticatedUser,
     authorizeRoles("admin"),
@@ -20,9 +20,10 @@ router
     createProduct
   );
 router
-  .route("/product/:id")
+  .route("/admin/product/:id")
   .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
-  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
-  .get(getProductDetails);
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+router.route("/product/:id").get(getProductDetails);
 
 module.exports = router;
